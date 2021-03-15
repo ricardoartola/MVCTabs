3051e8792c9a9e7dfc7663e4b0e18d5638a36aa4
Ricardo Artola <rartola@outlook.com>
2021-03-15 13:19:28 -0400

    Add project files.

---
+++ MVCTabs/Controllers/ApplicationsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTabs.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+
+        public ActionResult EducationDetails()
+        {
+            return View();
+        }
+
+        public ActionResult EnclosedDocuments()
+        {
+            return View();
+        }
+    }
+}
